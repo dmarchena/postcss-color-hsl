08eb5604642815ab1b0fd6141db0570a65e465b2
Dmitry Semigradsky <semigradskyd@gmail.com>
2017-05-15 10:28:33 +0300

    Update PostCSS

--- src/index.js
+++ src/index.js
@@ -5,7 +5,10 @@ import functionalNotation from './lib/hsl-functional-notation';
 function transformHsl(value) {
     return parser(value).walk(node => {
         /* istanbul ignore if */
-        if (node.type !== 'function' || (node.value !== 'hsl' && node.value !== 'hsla')) {
+        if (node.type !== 'function' ||
+            node.value !== 'hsl' &&
+            node.value !== 'hsla'
+        ) {
             return;
         }
         node.value = functionalNotation.legacy(parser.stringify(node));
@@ -13,13 +16,14 @@ function transformHsl(value) {
     }).toString();
 }
 
-module.exports = postcss.plugin('postcss-color-hsl', function (opts) {
-    opts = opts || {};
-
-    return function (root, result) {
+module.exports = postcss.plugin('postcss-color-hsl', function () {
+    return function (root) {
         root.walkDecls(decl => {
             /* istanbul ignore if */
-            if (!decl.value || (decl.value.indexOf('hsl(') === -1 && decl.value.indexOf('hsla(') === -1)) {
+            if (!decl.value ||
+                decl.value.indexOf('hsl(') === -1 &&
+                decl.value.indexOf('hsla(') === -1
+            ) {
                 return;
             }
             decl.value = transformHsl(decl.value);
